7702a4f76f1cb2b9276c4fd2a56f98ac5296b18a
Хилда Митева <mitewwaa@gmail.com>
2024-11-20 18:37:15 +0200

    added some css

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -6,12 +6,14 @@ import '../styles/LandingPage.css'
 const LandingPage: React.FC = () => {
   return (
     <div className='page'>
-      <div className='container'>
-        <div className='buttonContainer'>
+      <div className='pageContainer'>
+        <div className='textContainer'>
             <h1>Pocketly</h1>
             <h2>The modern way to manage your finances!</h2>
-            <button className='button'><Link to="/login">Log In</Link></button>
-            <button className='button'><Link to="/register">Register</Link></button>
+            <div className='buttonContainer'>
+              <button className='button'><Link to="/login">Log In</Link></button>
+              <button className='button'><Link to="/register">Register</Link></button>
+            </div>
         </div>
         <div className='imageContainer'>
             <img src='./images/OBJECTS.svg' id='image' alt='piggy-bank'/>
