6cf99ba5ce736939727ce5716e898ab453f0db3f
Хилда Митева <mitewwaa@gmail.com>
2024-12-26 18:31:22 +0200

    added some css

--- client/src/components/Budgets/BudgetItem.tsx
+++ client/src/components/Budgets/BudgetItem.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import BudgetData from '../../../../server/src/shared/interfaces/BudgetData';
 import AddTransactionToBudget from './AddTransactionToBudget';
+import { CiEdit } from 'react-icons/ci';
+import { FaPlus } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
 
 interface BudgetItemProps {
   budget: BudgetData;
@@ -16,22 +19,39 @@ const BudgetItem: React.FC<BudgetItemProps> = ({ budget, onEdit, onDelete, updat
     setIsModalOpen(!isModalOpen);
   };
 
+  const getBudgetTypeText = (type: string) => {
+    switch (type) {
+      case 'goal':
+        return 'Goal';
+      case 'category_limit':
+        return 'Category limit';
+      default:
+        return 'Unknown';
+    }
+  };
+
+  const budgetTypeText = getBudgetTypeText(budget.type);
+
   return (
     <li className="budgetItem">
       <h3 className="budgetName">{budget.name}</h3>
-      <p className="budgetType">Type: {budget.type}</p>
-      <p className="categoryName">Category: {budget.category_name}</p>
+      <div className='budgetInfo'>  
+        <p className="budgetType">Type: {budgetTypeText}</p>
+        <p className="categoryName">Category: {budget.category_name}</p>
+      </div>
       <div className="amountContainer">
         <p className="amount">Amount: {budget.amount}</p>
         <p className="amount">Amount left: {budget.amount_left}</p>
       </div>
       <div className="dateContainer">
-        <p className="date">Start Date: {new Date(budget.start_date).toLocaleDateString()}</p>
-        <p className="date">End Date: {new Date(budget.end_date).toLocaleDateString()}</p>
+        <p className="budgetDate">Start Date: {new Date(budget.start_date).toLocaleDateString()}</p>
+        <p className="budgetDate">End Date: {new Date(budget.end_date).toLocaleDateString()}</p>
       </div>
-      <button onClick={() => onEdit(budget)}>Edit</button>
-      <button onClick={() => onDelete(budget.id)}>Delete</button>
-      <button onClick={toggleModal}>Add Expense</button> 
+      <div className='buttonsContainer'>
+      <button onClick={() => onDelete(budget.id)} className='deleteBtn btn'> <MdDelete className='btnIcon'/><p className='text'>Delete</p></button>
+        <button onClick={() => onEdit(budget)} className='editBtn btn'><CiEdit className='btnIcon'/> Edit</button>
+        <button onClick={toggleModal} className='expenseBtn btn'><FaPlus className='btnIcon' /> Add Expense</button> 
+      </div>
 
       <AddTransactionToBudget
         budget={budget}
